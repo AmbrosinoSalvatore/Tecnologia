84eae02837715ef39d77db7a7337eb6b3702cc04
sasyvelobelo <ambrosino.salvatore@iisgalvanimi.edu.it>
2023-08-19 16:44:54 +0000

    punto5a

--- base/base/src/app/app-routing.module.ts
+++ base/base/src/app/app-routing.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { SearchPokemonComponent } from './search-pokemon/search-pokemon.component';
 import { ItemPokemonComponent } from './item-pokemon/item-pokemon.component';
@@ -8,6 +8,10 @@ const routes: Routes = [
     path:'',
     component:SearchPokemonComponent
     
+  }
+  ,{
+    path:'pokemon/:id',
+    component:ItemPokemonComponent
   }
 
 ];
--- base/base/src/app/item-pokemon/item-pokemon.component.ts
+++ base/base/src/app/item-pokemon/item-pokemon.component.ts
@@ -1,10 +1,28 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { ActivatedRoute, ParamMap, Router } from '@angular/router';
+import { PokemonApiManegerService } from '../pokemon-api-maneger.service';
 
 @Component({
   selector: 'app-item-pokemon',
   templateUrl: './item-pokemon.component.html',
   styleUrls: ['./item-pokemon.component.css']
 })
-export class ItemPokemonComponent {
+export class ItemPokemonComponent implements OnInit {
 @Input() data:any={}
+id:string|undefined=''
+constructor(private router:Router,private route:ActivatedRoute,private api:PokemonApiManegerService){
+
+}
+ngOnInit(): void {
+  this.route.paramMap.subscribe((params: ParamMap) => {
+    this.id = params.get('id')?.toString();
+
+    if (!this.id) this.router.navigate(['/']);
+    else {
+this.api.getPokemon(this.id).subscribe((data)=>{this.data=data})
+    }
+  });
+}
+
+
 }
