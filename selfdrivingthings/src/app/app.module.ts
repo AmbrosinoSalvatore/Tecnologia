fcffa51030912dd562aab46306e822c6f164e42f
sasyvelobelo <ambrosino.salvatore@iisgalvanimi.edu.it>
2023-08-23 20:29:31 +0000

    drive

---
+++ selfdrivingthings/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { DrivingListComponent } from './driving-list/driving-list.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    DrivingListComponent
+  ],
+  imports: [
+    BrowserModule,HttpClientModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ selfdrivingthings/src/app/driving-list/driving-list.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-driving-list',
+  templateUrl: './driving-list.component.html',
+  styleUrls: ['./driving-list.component.css']
+})
+export class DrivingListComponent {
+
+}
